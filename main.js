20ee8a632ceb254442363268f4b0ff6eb78f3e27
Luke Schuyler <lukeschuyler1@gmail.com>
2017-03-09 15:03:15 -0600

    fully populated bangazon database. returned query of name and price of products

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ db.run('CREATE TABLE IF NOT EXISTS products (ProductId INTEGER PRIMARY KEY AUTOI
 db.run('CREATE TABLE IF NOT EXISTS orders (OrderId INTEGER PRIMARY KEY AUTOINCREMENT, CustomerId INT, PaymentOptionsId INT, PaidInFull TEXT)');
 db.run('CREATE TABLE IF NOT EXISTS order_line_items (OrderLineId INTEGER PRIMARY KEY AUTOINCREMENT, OrderId INT, ProductId INT)');
 
-const populateCustomers= () => {
+const populateCustomers = () => {
 	const { customers } = require('./JSON/customers.json')
 
 	customers.forEach(cust => {
@@ -25,6 +25,55 @@ const populateCustomers= () => {
 	})
 }
 
-populateCustomers()
+const populateProducts = () => {
+	const { products } = require('./JSON/products.json')
 
-// db.run('DROP TABLE customer')
+	products.forEach(prod => {
+		db.run(`INSERT INTO products VALUES (
+			null,
+			"${prod.name}",
+			${prod.price})`)
+	})
+}
+
+const populateOrders = () => {
+	const { orders } = require('./JSON/orders.json')
+
+	orders.forEach(ord => {
+		db.run(`INSERT INTO orders VALUES (
+			null,
+			${ord.customerId},
+			${ord.paymentOptionId},
+			"${ord.paidInFull}")`)
+	})
+}
+
+const populatePaymentOptions = () => {
+	const { paymentOptions } = require('./JSON/payment_options.json')
+
+	paymentOptions.forEach(pay => {
+		db.run(`INSERT INTO payment_options VALUES (
+			null,
+			"${pay.name}",
+			${pay.accountNumber})`)
+	})
+}
+
+const populateOrderLine = () => {
+	const { orderLine } = require('./JSON/order_line.json')
+
+	orderLine.forEach(line => {
+		db.run(`INSERT INTO order_line_items VALUES (
+			null,
+			${line.orderId},
+			${line.productId})`)
+	})
+}
+
+// populateCustomers();
+// populateProducts();
+// populateOrders();
+// populatePaymentOptions()
+populateOrderLine();
+
+// db.run('DROP TABLE orders')
